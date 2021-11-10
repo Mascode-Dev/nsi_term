f3b793bc73401b7ef6231f0a82283a6db5c9192e
Mascode-Dev <58656691+Mascode-Dev@users.noreply.github.com>
2021-11-10 21:41:16 +0100

    Delete pgcd.py

--- pgcd.py
+++
@@ -1,8 +0,0 @@
-def pgcd(a,b):
-    """pgcd(a,b): calcul du 'Plus Grand Commun Diviseur' entre les 2 nombres entiers a et b"""
-    if b==0:
-        return a
-    else:
-        r=a%b #Reste de la division de a par b
-        return pgcd(b,r) # -> b=rq+r'
-
